4ec6f0bbf8edc8a262fd954014d22ffb6ffc18b0
tameem akhtar <akhtartamimm@gmail.com>
2026-02-08 17:49:04 +0530

    first commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 let express = require ('express')
+let path = require ('path')
 let app = express()
 let cors = require('cors')
+app.use(express.static('./public'))
 let authrouter = require('./router/authroutes')
 app.use(cors({
      origin: 'http://localhost:5173', 
@@ -8,4 +10,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use('/api/auth',authrouter)
+app.use('*name',(req,res)=> {
+    res.sendFile(path.join(__dirname,'..','/public/index.html'))
+})
 module.exports = app
